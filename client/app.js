1cf7ff9e27d38d3f755a25e467a6fa739a9b7d34
Rolando Cosme <rolandocosme@Rolandos-MBP.home>
2016-05-01 22:42:14 -0400

    began creating home route and folder structure

---
+++ client/app.js
@@ -0,0 +1,18 @@
+angular.module('ladybug', [
+  'ui.router'
+])
+.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'partials/partial-home.html',
+        controller: 'homeController'
+      });
+
+  $locationProvider.html5Mode(true);
+}]);
+
+
+alert('hi');
--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var path = require('path');
 
 var port = process.env.PORT || 8080;
 
+// var app = module.exports = express();
+var app = express();
+
+// app.use('/', router);
+app.use(express.static('client'));
+
 http.listen(port, function(){
   console.log("Magic on Port " + port);
 });
