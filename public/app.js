a28574074d9da60c6f9df76a69c3a75433cf1205
RolandoCosme <Rolando.Cosme@gmail.com>
2016-06-09 22:07:31 -0400

    testing new angular grid system

--- public/app.js
+++ public/app.js
@@ -12,7 +12,7 @@ angular.module('Ladybug', [
           // controller: 'galleryController'
       })
       .when('/gallery2',{
-          templateUrl: '/partials/test-partial-gallery.html',
+          templateUrl: '/partials/partial-grid.html',
           controller: 'galleryController'
       })
       .otherwise({
