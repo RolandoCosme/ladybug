960d8f2d3ea77b69ec6e6390c45ed52e7652f8e0
Rolando Cosme <rolandocosme@Rolandos-MacBook-Pro.local>
2016-07-28 13:12:42 -0400

    added php form file

--- public/partials/partial-form.php
+++ public/partials/partial-form.php
@@ -1,11 +1,11 @@
-<?php
+<!-- <?php
 if (empty($_POST) === false) {
   echo "<pre>", print_r($_POST, true), "</pre>";
 }
-?>
+?> -->
 
 <h1>Enrollment Form</h1>
-<form action="" method="post">
+<form action="send.php" method="post">
   <p>
     <label for="name">Name:</label><br>
     <input type="text" name="name" id="name">
@@ -16,7 +16,7 @@ if (empty($_POST) === false) {
   </p>
   <p>
     <label for="message">Message:</label></br>
-    <textarea name="message" id="message"></textarea>
+    <textarea name="message" id="message" rows="10" cols="50"></textarea>
   </p>
   <p>
     <input type="submit" value="Submit">
