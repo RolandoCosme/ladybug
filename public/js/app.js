6a3308e8e255a7b07651ed76c6a53506cb215fa6
Rolando Cosme <rolando.cosme@gmail.com>
2017-01-06 19:18:23 -0500

    chahged routes to make heroku load work

--- public/js/app.js
+++ public/js/app.js
@@ -5,38 +5,38 @@ angular.module('Ladybug', [
 
     $routeProvider
       .when('/home', {
-        templateUrl: '/partials/partial-home.html'
+        templateUrl: './partials/partial-home.html'
       })
       .when('/gallery',{
-          templateUrl: '/partials/partial-gallery.html',
+          templateUrl: './partials/partial-gallery.html',
       })
       .when('/practical-life',{
-          templateUrl: '/partials/partial-practical.html',
+          templateUrl: './partials/partial-practical.html',
       })
       .when('/sensorial',{
-          templateUrl: '/partials/partial-sensorial.html',
+          templateUrl: './partials/partial-sensorial.html',
       })
       .when('/mathematics',{
-          templateUrl: '/partials/partial-mathematics.html',
+          templateUrl: './partials/partial-mathematics.html',
       })
       .when('/language',{
-          templateUrl: '/partials/partial-language.html',
+          templateUrl: './partials/partial-language.html',
       })
       .when('/science',{
-          templateUrl: '/partials/partial-science.html',
+          templateUrl: './partials/partial-science.html',
       })
       .when('/geography',{
-          templateUrl: '/partials/partial-geography.html',
+          templateUrl: './partials/partial-geography.html',
       })
       .when('/art',{
-          templateUrl: '/partials/partial-art.html',
+          templateUrl: './partials/partial-art.html',
       })
       .when('/enrollment',{
-          templateUrl: '/partials/partial-enrollment.html',
+          templateUrl: './partials/partial-enrollment.html',
           controller: 'submitController.js'
       })
       .when('/contact',{
-          templateUrl: '/partials/partial-contact.html',
+          templateUrl: './partials/partial-contact.html',
           // controller: 'mailController.js'
       })
       .otherwise({
