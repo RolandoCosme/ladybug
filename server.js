e6f3a31c621fac76e3c7f69eebdcce703ed1da45
Rolando Cosme <rolando.cosme@gmail.com>
2017-01-09 12:52:42 -0500

    added path npm package to try and solve heroku issue

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 var http = require('http').Server(app);
 var router = express.Router();
 var logger = require('morgan');
@@ -11,7 +12,8 @@ var PORT = process.env.NODE_ENV || 8080;
 
 app.use(logger ( 'dev' ));
 // app.use('/', router);
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, function(){
   console.log("Listening on " + PORT);
